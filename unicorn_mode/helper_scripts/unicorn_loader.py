d167a3986e0ca35e341cb5ff63225382b211e67d
jbachell <john.bacheller1@gmail.com>
2019-06-19 12:52:24 -0400

    implimented trace instruction using Capstone to dumb assembly :), also changed to repeating STACK POINTER registers in map arrays, lots of white space also removed due to text editor (atom)

--- unicorn_mode/helper_scripts/unicorn_loader.py
+++ unicorn_mode/helper_scripts/unicorn_loader.py
@@ -253,37 +253,17 @@ class AflUnicornEngine(Uc):
         for reg in sorted(self.__get_register_map(self._arch_str).items(), key=lambda reg: reg[0]):
             print(">>> {0:>4}: 0x{1:016x}".format(reg[0], self.reg_read(reg[1])))
 
-    def dump_stack(self, window=5):
-        arch = self.get_arch()
-        mode = self.get_mode()
-        # Get stack pointers and bit sizes for given architecture
-        if arch == UC_ARCH_X86 and mode == UC_MODE_64:
-            stack_ptr_addr = self.reg_read(UC_X86_REG_RSP)
-            bit_size = 8
-        elif arch == UC_ARCH_X86 and mode == UC_MODE_32:
-            stack_ptr_addr = self.reg_read(UC_X86_REG_ESP)
-            bit_size = 4
-        elif arch == UC_ARCH_ARM64:
-            stack_ptr_addr = self.reg_read(UC_ARM64_REG_SP)
-            bit_size = 8
-        elif arch == UC_ARCH_ARM:
-            stack_ptr_addr = self.reg_read(UC_ARM_REG_SP)
-            bit_size = 4
-        elif arch == UC_ARCH_MIPS:
-            stack_ptr_addr = self.reg_read(UC_MIPS_REG_SP)
-            bit_size = 4
+    # TODO: Make this dynamically get the stack pointer register and pointer width for the current architecture
+    """
+    def dump_stack(self, window=10):
         print(">>> Stack:")
-        # Make sure window is not too large to go past stack
+        stack_ptr_addr = self.reg_read(UC_X86_REG_RSP)
         for i in xrange(-window, window + 1):
-            # Increament address and print stack
-            addr = stack_ptr_addr + (i*bit_size)
-            if bit_size == 4:
-                print("{0}0x{1:08x}: 0x{2:08x}".format('SP->' if i == 0 else '    ', addr, \
-                    struct.unpack('<I', self.mem_read(addr, bit_size))[0]))
-            else:
-                print("{0}0x{1:016x}: 0x{2:016x}".format('SP->' if i == 0 else '    ', addr, \
-                    struct.unpack('<Q', self.mem_read(addr, bit_size))[0]))
-
+            addr = stack_ptr_addr + (i*8)
+            print("{0}0x{1:016x}: 0x{2:016x}".format( \
+                'SP->' if i == 0 else '    ', addr, \
+                struct.unpack('<Q', self.mem_read(addr, 8))[0]))
+    """
     #-----------------------------
     #---- Loader Helper Functions
 
@@ -417,7 +397,6 @@ class AflUnicornEngine(Uc):
                 "r14":    UC_X86_REG_R14,
                 "r15":    UC_X86_REG_R15,
                 "rip":    UC_X86_REG_RIP,
-                "rsp":    UC_X86_REG_RSP,
                 "efl":    UC_X86_REG_EFLAGS,
                 "cs":     UC_X86_REG_CS,
                 "ds":     UC_X86_REG_DS,
@@ -436,7 +415,6 @@ class AflUnicornEngine(Uc):
                 "ebp":    UC_X86_REG_EBP,
                 "esp":    UC_X86_REG_ESP,
                 "eip":    UC_X86_REG_EIP,
-                "esp":    UC_X86_REG_ESP,
                 "efl":    UC_X86_REG_EFLAGS,
                 # Segment registers removed...
                 # They caused segfaults (from unicorn?) when they were here
