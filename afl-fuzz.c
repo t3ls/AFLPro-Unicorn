5542676f3f6d19df8bd4e81badd3986355edff22
t3ls <t3ls.bupt@gmail.com>
2021-05-11 13:38:54 +0800

    chore(aflpro): change banner

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -4000,7 +4000,7 @@ static void show_stats(void) {
 
     sprintf(tmp + banner_pad, "%s " cLCY VERSION cLGN
                               " (%s)",  crash_mode ? cPIN "peruvian were-rabbit" :
-                                        cYEL "american fuzzy lop", use_banner);
+                                        cYEL "AFLPro - Unicorn", use_banner);
 
     SAYF("\n%s\n\n", tmp);
 
